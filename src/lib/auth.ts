a98edee583135362b1d06c80640c10fe3fcbc0ad
asraful-07 <asraful0508@gmail.com>
2026-01-10 11:17:31 +0600

    auth  done

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-import { twoFactor } from "better-auth/plugins";
+import { role, twoFactor } from "better-auth/plugins";
 import { Resend } from "resend";
-import { admin } from "better-auth/plugins";
-import { adminRole, userRole } from "./permission";
 
 const resend = new Resend("re_Vv9NuspD_43ocZQ3jvFaEevnm86RbRWfV");
 
@@ -14,6 +12,20 @@ export const auth = betterAuth({
     provider: "postgresql",
   }),
   trustedOrigins: [process.env.APP_URL!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        defaultValue: "Unknown",
+        required: false,
+      },
+    },
+  },
   emailAndPassword: {
     enabled: true,
     autoSignIn: false,
@@ -24,19 +36,10 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-      redirectURI: `${process.env.FRONTEND_URL}/api/auth/callback/github`,
     },
   },
 
   plugins: [
-    admin({
-      adminRoles: ["admin", "user"],
-      defaultRole: "user",
-      roles: {
-        admin: adminRole,
-        user: userRole,
-      },
-    }),
     twoFactor({
       otpOptions: {
         period: 2,
